d5f40a95a12e79f9a0027867ce662405faaaab9f
scontreraslopez <s.contreraslopez@edu.gva.es>
2026-01-28 22:53:23 +0100

    now nullable in UiState

--- app/src/main/java/net/iessochoa/sergiocontreras/racetraitpicker/ui/screens/RaceCreationUiState.kt
+++ app/src/main/java/net/iessochoa/sergiocontreras/racetraitpicker/ui/screens/RaceCreationUiState.kt
@@ -5,9 +5,10 @@ import net.iessochoa.sergiocontreras.racetraitpicker.model.RaceOption
 import net.iessochoa.sergiocontreras.racetraitpicker.model.TraitsCategories
 
 data class RaceCreationUiState (
-    val populationOptions: List<RaceOption> = RaceTraitsRepository.getRaceOptionsByCategory(TraitsCategories.POPULATION),
-    val farmingOptions: List<RaceOption> = RaceTraitsRepository.getRaceOptionsByCategory(TraitsCategories.FARMING),
-    val selectedPopulationOption: RaceOption = populationOptions[1],
-    val selectedFarmingOption: RaceOption = farmingOptions[1],
+    val populationOptions: List<RaceOption> = emptyList(),
+    val farmingOptions: List<RaceOption> = emptyList(),
+    val selectedPopulationOption: RaceOption? = null,
+    val selectedFarmingOption: RaceOption? = null,
     val remainingPoints: Int = 10
 )
+
